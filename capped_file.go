3323116febbbbda9cf1a4e1fbccf48dce8ecfd74
Martin Holst Swende <martin@swende.se>
2023-07-12 14:12:31 +0200

    implement capped files

---
+++ capped_file.go
@@ -0,0 +1,191 @@
+// bagdb: Simple datastorage
+// Copyright 2021 billy authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+package billy
+
+import (
+	"fmt"
+	"os"
+)
+
+// cappedFile has an API-surface as if it were one file, but maps
+// to a set of files on disk. These files are all capped in size
+// to maxFileSize.
+type cappedFile struct {
+	cap   uint64
+	files []*os.File
+}
+
+// newCappedFile creates a cappedFile
+func newCappedFile(basename string, nFiles int, cap uint64, readonly bool) (*cappedFile, error) {
+	flags := os.O_RDWR | os.O_CREATE
+	if readonly {
+		flags = os.O_RDONLY
+	}
+	var files []*os.File
+	for i := 0; i < nFiles; i++ {
+		var (
+			f   *os.File
+			err error
+		)
+		if i == 0 {
+			f, err = os.OpenFile(basename, flags, 0666)
+		} else {
+			f, err = os.OpenFile(fmt.Sprintf("%v.cap.%d", basename, i), flags, 0666)
+		}
+		if err != nil {
+			// Clean-up: close opened files
+			for _, f := range files {
+				f.Close()
+			}
+			return nil, err
+		}
+		files = append(files, f)
+	}
+	return &cappedFile{
+		cap:   cap,
+		files: files,
+	}, nil
+}
+
+// Write implements io.Writer
+func (cf *cappedFile) Write(p []byte) (n int, err error) {
+	return cf.WriteAt(p, 0)
+}
+
+// WriteAt writes len(b) bytes to the file starting at byte offset off.
+// It returns the number of bytes written and an error, if any.
+// WriteAt returns a non-nil error when n != len(b).
+func (cf *cappedFile) WriteAt(data []byte, off int64) (n int, err error) {
+	var (
+		offset      = uint64(off)
+		written     int // no of bytes read
+		totalLength = len(data)
+		fileNum     = offset / cf.cap
+	)
+	if fileNum >= uint64(len(cf.files)) {
+		return 0, ErrBadIndex
+	}
+	for ; written < totalLength; fileNum++ {
+		offset = offset % cf.cap
+		length := uint64(len(data))
+		if offset+length > cf.cap {
+			// Write continuing in the next
+			length = cf.cap - offset
+		}
+		//fmt.Printf("WriteAt file-%d at %d <- b[:%d]\n", fileNum, offset, length)
+		if n, err := cf.files[fileNum].WriteAt(data[:length], int64(offset)); err != nil {
+			return written + n, err
+		}
+		data = data[length:]
+		written += int(length)
+		offset += length
+	}
+	return written, nil
+}
+
+// Read implements io.Reader
+func (cf *cappedFile) Read(p []byte) (n int, err error) {
+	return cf.ReadAt(p, 0)
+}
+
+// ReadAt reads len(b) bytes from the file(s) starting at byte offset off.
+// It returns the number of bytes read and the error, if any.
+func (cf *cappedFile) ReadAt(b []byte, off int64) (n int, err error) {
+	var (
+		offset  = uint64(off)
+		read    int // no of bytes read
+		fileNum = offset / cf.cap
+	)
+	if fileNum >= uint64(len(cf.files)) {
+		return 0, ErrBadIndex
+	}
+	for ; read < len(b); fileNum++ {
+		offset = offset % cf.cap
+		length := uint64(len(b) - read) // no of bytes to read this iteration
+		if offset+length > cf.cap {
+			length = cf.cap - offset
+		}
+		//fmt.Printf("ReadAt  file-%d at %d -> b[%d:%d]\n", fileNum, offset, read, length)
+		if n, err := cf.files[fileNum].ReadAt(b[read:uint64(read)+length], int64(offset)); err != nil {
+			return read + n, err
+		}
+		read += int(length)
+		offset += length
+	}
+	return read, nil
+}
+
+// Sync calls *os.File Sync on the backing-files.
+func (cf *cappedFile) Sync() error {
+	var err error
+	for _, f := range cf.files {
+		if e := f.Sync(); e != nil && err == nil {
+			err = e
+		}
+	}
+	return err
+}
+
+// Close closes all files.
+func (cf *cappedFile) Close() error {
+	var err error
+	for _, f := range cf.files {
+		if e := f.Close(); e != nil && err == nil {
+			err = e
+		}
+	}
+	return err
+}
+
+// Truncate changes the size of the file.
+func (cf *cappedFile) Truncate(size int64) error {
+	var err error
+	for i, f := range cf.files {
+		// Files below the truncation limit are ignored.
+		if uint64(i+1)*cf.cap <= uint64(size) {
+			continue
+		}
+		// Files fully above the truncation limit are truncated to zero.
+		if uint64(i)*cf.cap > uint64(size) {
+			if e := f.Truncate(0); e != nil && err == nil {
+				err = e
+			}
+			continue
+		}
+		fSize := size % int64(cf.cap)
+		//fmt.Printf("Truncate file-%d to %d (total size %d)\n", i, fSize, size)
+		if e := f.Truncate(fSize); e != nil && err == nil {
+			err = e
+		}
+	}
+	return err
+}
+
+type Sizer interface {
+	Size() int64 // length in bytes for regular files; system-dependent for others
+}
+type sizeDummy struct {
+	size int64
+}
+
+func (s *sizeDummy) Size() int64 {
+	return s.size
+}
+
+func (cf *cappedFile) Stat() (Sizer, error) {
+	var size int64
+	var err error
+	for _, f := range cf.files {
+		finfo, e := f.Stat()
+		if e != nil && err != nil {
+			err = e
+		}
+		if finfo != nil {
+			size += finfo.Size()
+		}
+	}
+	return &sizeDummy{size}, err
+
+}
---
+++ capped_file_test.go
@@ -0,0 +1,134 @@
+// bagdb: Simple datastorage
+// Copyright 2021 billy authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+package billy
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+// wipe deletes the files.
+func wipe(t *testing.T, cf *cappedFile) {
+	t.Helper()
+	var err error
+	for _, f := range cf.files {
+		if e := os.RemoveAll(f.Name()); e != nil && err == nil {
+			err = e
+		}
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func diskSize(t *testing.T, cf *cappedFile) int {
+	t.Helper()
+	var size int
+	for _, f := range cf.files {
+		finfo, err := f.Stat()
+		if err != nil {
+			t.Fatal(err)
+		}
+		size += int(finfo.Size())
+		//t.Logf("file %d size %d total %d", j, finfo.Size(), have)
+	}
+	return size
+}
+
+func TestWriteAt(t *testing.T) {
+	// Max filesize: 50 bytes.
+	// 10 individual files.
+	// Mox storage capacity: 500 bytes.
+	f, err := newCappedFile("multifile-test", 10, 50, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { wipe(t, f) })
+
+	{
+		// Writing data at offset 55 means that it should write into
+		// the second file, five bytes in.
+		// [0,0,0,0,0,m,o,o,h]
+		want := []byte("mooh")
+		if _, err := f.WriteAt(want, 55); err != nil {
+			t.Fatal(err)
+		}
+		have := make([]byte, 4)
+		if _, err := f.ReadAt(have, 55); err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(have, want) {
+			t.Fatalf("have %x want %x", have, want)
+		}
+	}
+
+	{
+		// Test data that starts at one file, fully saturates a second, and
+		// ends a bit into the third.
+		want := []byte("miao01234567890123456789012345678901234567890123456789woof")
+		if _, err := f.WriteAt(want, 96); err != nil {
+			t.Fatal(err)
+		}
+		have := make([]byte, 58)
+		if _, err := f.ReadAt(have, 96); err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(have, want) {
+			t.Fatalf("have %x want %x", have, want)
+		}
+	}
+}
+
+func TestTruncate(t *testing.T) {
+	// Max filesize: 50 bytes.
+	// 10 individual files.
+	// Mox storage capacity: 500 bytes.
+	f, err := newCappedFile("multifile-test", 10, 50, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { wipe(t, f) })
+	// Fill with data
+	if _, err := f.WriteAt(make([]byte, 470), 20); err != nil {
+		t.Fatal(err)
+	}
+	// The total size of all files should == 490
+	if have, want := diskSize(t, f), 490; have != want {
+		t.Fatalf("have %d want %d", have, want)
+	}
+	for i := 480; i > 0; i -= 10 {
+		if err := f.Truncate(int64(i)); err != nil {
+			t.Fatal(err)
+		}
+		// The total size of all files should == i
+		if have, want := diskSize(t, f), i; have != want {
+			t.Fatalf("have %d want %d", have, want)
+		}
+	}
+}
+
+func TestReadonly(t *testing.T) {
+	// Create in readonly -- should fail
+	f, err := newCappedFile("multifile-ro-test", 10, 50, true)
+	if err == nil {
+		t.Fatal("want error trying to create files in readonly, got none")
+	}
+	// Create in RW, should be ok
+	f, err = newCappedFile("multifile-ro-test", 10, 50, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f.Close()
+	// The files now exist, so should be ok to open
+	f, err = newCappedFile("multifile-ro-test", 10, 50, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { wipe(t, f) })
+	if _, err := f.WriteAt([]byte("mooh"), 55); err == nil {
+		t.Fatalf("want error trying to write files in readonly, got none")
+	}
+}
